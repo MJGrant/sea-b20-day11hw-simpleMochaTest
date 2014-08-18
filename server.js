8de764bd826c83fd674fb4c77196e57ab3cfa99b
Mandi Grant <mandi018@gmail.com>
2014-08-17 18:29:15 -0700

    First pass on routes and mocha tests, now debugging mocha tests

---
+++ server.js
@@ -0,0 +1,54 @@
+//Simple JSON database app
+//To use: 
+//$ server.js
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var http = require('http');
+
+app.use(bodyParser.json());
+
+var count = 10;
+
+app.get('/', function(req, res) {
+  res.send('Welcome to Grubburg! Use /grubs/random to see a random grub, or /grubs/count to see how many grubs there are total.');
+});
+
+var grubs = [
+  { grub : 'Wy', text : "Why Wy wall?"},
+  { grub : 'Yummy', text : "YumMY!"},
+  { grub : 'Blizzy', text : "BIG AND PLUSHY"},
+  { grub : 'Annie', text : "FIVE!"},
+  { grub : 'Shy', text : "Why be Shy when Wy shies behind Shy?"},
+  { grub : 'Terry', text : "I hate you all >:("},
+  { grub : 'Gadget', text : "Gadget isn't a grub"},
+  { grub : 'Flake', text : "Flakerbake"},
+];
+
+app.get('/', function(req, res) {
+  res.json(grubs);
+});
+
+app.get('/grubs/random', function(req, res) {
+  var id = Math.floor(Math.random() * grubs.length);
+  var grub = grubs[id];
+  res.json(grub);
+});
+
+app.get('/grubs/count', function(req,res) {
+  res.json({
+    count:count,
+    message: "Wow, there are " + count + ' grubs!'
+  });
+});
+
+app.get('/grubs/add/:num', function(req, res) {
+  count += parseInt(req.params.num);
+  res.json({
+    add: req.params.num,
+    message: 'YAY! You just added ' + req.params.num + ' new grubs!'
+  });
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ tests/mocha/simple-mocha-tests.js
@@ -0,0 +1,39 @@
+var server = require('../..server');
+var chai = require('chai');
+var chaihttp = require('chai-http');
+chai.use(chaihttp);
+var expect = chai.expect;
+
+describe('grubRoutes', function() {
+  var url = 'http://localhost:3000';
+
+  it('gets a random grub', function (done) {
+    chai.request(url)
+      .get('/grub/random')
+      .res(function(res) {
+        expect(res).to.have.status(200);
+        expect(Array.isArray(res.body)).to.be.true;
+        expect(res.body[0]).to.have.property('grub');
+        done();
+      });
+  });
+
+  it('checks if the count is a number', function (done) {
+    chai.request(url)
+    .res(function(res) {
+      expect(res).to.have.status(200);
+      expect(grubs.count).to.be.a('number');
+      done();
+    });
+  });
+
+  it('should update the grub count', function (done) {
+    chai.request(url)
+      .post('/grub/count/1')
+      .res(function(res) {
+        expect(res).to.have.status(200);
+        expect(grubs.count).to.eql(11);
+        done();
+      });
+  });
+});
